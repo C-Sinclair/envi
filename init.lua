0971569ea6dcbeb525f1579ab8dd8c8344e7a954
Conor Sinclair <conor@sinclair.software>
2023-01-03 12:25:23 +0000

    better reload function

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 require("envi").setup()
 
 vim.api.nvim_create_user_command("EnviReload", function()
+  -- clear any loaded packages, so submodules refresh with config refresh.
+  for name, _ in pairs(package.loaded) do
+    if name:match "^envi" then
+      package.loaded[name] = nil
+    end
+  end
   R("envi").setup()
 end, {})
