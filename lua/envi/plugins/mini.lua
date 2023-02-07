19d41c0b2cf37fc10ab869e1f95bca6b1670eb91
Conor Sinclair <conor@sinclair.software>
2023-02-07 17:45:07 +0000

    added mini plugins

--- lua/envi/plugins/mini.lua
+++ lua/envi/plugins/mini.lua
@@ -2,17 +2,17 @@ local M = {}
 
 M.setup = function()
   require("mini.ai").setup()
+  require("mini.move").setup()
   --[[ require("mini.surround").setup() ]]
-
   local MiniBuf = require "mini.bufremove"
   MiniBuf.setup {}
   -- hide the current buffer
   vim.keymap.set("n", "<leader>X", function()
-    MiniBuf.unshow()
+    MiniBuf.unshow(0)
   end)
   -- close the current buffer
   vim.keymap.set("n", "<leader>x", function()
-    MiniBuf.delete()
+    MiniBuf.delete(0, false)
   end)
 end
 
