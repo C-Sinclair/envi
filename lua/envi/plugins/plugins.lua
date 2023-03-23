321809711a5dbf5b23cd4738d09030db87f139af
Conor Sinclair <conor@sinclair.software>
2023-03-23 10:47:14 +0000

    installed copilot

--- lua/envi/plugins/plugins.lua
+++ lua/envi/plugins/plugins.lua
@@ -589,4 +589,42 @@ return require("packer").startup(function(use)
       }
     end,
   }
+
+  use {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-y>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "right",
+            ratio = 0.25,
+          },
+        },
+      }
+    end,
+  }
 end)
